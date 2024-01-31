d03fbd26fa7f490980f66ebc29ad9c9e9b330433
RobertoScarci <robertoscarci.02@gmail.com>
2024-01-31 14:48:21 +0100

    first commit

---
+++ resources/views/header.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+</head>
+<body>
+    <main>
+        <h1>
+            Live or Die?
+        </h1>
+        <img src="https://movieplayer.net-cdn.it/t/images/2023/09/19/anakin_ahsoka_jpg_1600x900_crop_q85.jpg" alt="">
+    </main>   
+</body>
+</html>
